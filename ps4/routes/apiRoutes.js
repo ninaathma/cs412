c74900a210c811eba1fc278ba5028a661c909230
Nina Athma <nina.athma@gmail.com>
2023-12-12 11:51:44 -0500

    added 3 buttons

--- ps4/routes/apiRoutes.js
+++ ps4/routes/apiRoutes.js
@@ -1,30 +1,48 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
-const fetch = require('node-fetch');
+const config = require('../config'); // Adjust the path as needed
 
-const apiKey = '6f3f7552c9msh8236d150d7b3cf7p184044jsnb274092385a4'; // Replace with your API key
+const apiKey = config.apiKey;
 
-// Route with Promises
-router.post('/form', (req, res) => {
-    const { search } = req.body;
+router.post('/promise', handleWeatherRequest);
+router.post('/asyncawait', handleWeatherRequestAsyncAwait);
+router.post('/callback', handleWeatherRequestCallback);
 
-    if (!search) {
-        return res.status(400).send('Please enter a city.');
+function handleWeatherRequest(req, res) {
+    const { cityPromise } = req.body;
+    makeApiRequestAndRender(res, cityPromise);
+}
+
+async function handleWeatherRequestAsyncAwait(req, res) {
+    const { cityAsyncAwait } = req.body;
+    try {
+        const response = await makeApiRequest(cityAsyncAwait);
+        renderTemplateWithData(res, response);
+    } catch (error) {
+        res.status(500).send('Error fetching data');
     }
+}
 
+function handleWeatherRequestCallback(req, res) {
+    const { cityCallback } = req.body;
+    makeApiRequest(cityCallback)
+        .then((response) => renderTemplateWithData(res, response))
+        .catch(() => res.status(500).send('Error fetching data'));
+}
+
+function makeApiRequest(city) {
     const options = {
         method: 'GET',
         url: 'https://weatherapi-com.p.rapidapi.com/current.json',
-        qs: { q: search },
+        qs: { q: city },
         headers: {
             'X-RapidAPI-Key': apiKey,
             'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
         },
     };
 
-    // Wrap the request in a Promise
-    const apiPromise = new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         request(options, (error, response, body) => {
             if (error) {
                 reject(error);
@@ -33,57 +51,16 @@ router.post('/form', (req, res) => {
             }
         });
     });
+}
 
-    apiPromise
-        .then((data) => {
-            res.render('result', { data });
-        })
-        .catch((error) => {
-            res.status(500).send('Error fetching data');
-        });
-});
+function makeApiRequestAndRender(res, city) {
+    makeApiRequest(city)
+        .then((response) => renderTemplateWithData(res, response))
+        .catch(() => res.status(500).send('Error fetching data'));
+}
 
-// Route with async/await
-router.post('/asyncawait', async (req, res) => {
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': apiKey,
-            'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
-        },
-    };
+function renderTemplateWithData(res, data) {
+    res.render('result', { data });
+}
 
-    try {
-        const response = await fetch(
-            'https://weatherapi-com.p.rapidapi.com/current.json?q=53.1,-0.13',
-            options
-        );
-        const data = await response.json();
-        res.render('result', { data });
-    } catch (error) {
-        res.status(500).send('Error fetching data');
-    }
-});
-
-// Route with callback
-router.post('/callback', (req, res) => {
-    const options = {
-        method: 'GET',
-        url: 'https://weatherapi-com.p.rapidapi.com/current.json',
-        qs: { q: '53.1,-0.13' },
-        headers: {
-            'X-RapidAPI-Key': apiKey,
-            'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
-        },
-    };
-
-    // Using callback for the async API call
-    request(options, (error, response, body) => {
-        if (error) {
-            res.status(500).send('Error fetching data');
-        } else {
-            res.render('result', { data: body });
-        }
-    });
-});
 module.exports = router;
