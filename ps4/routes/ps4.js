1f64d22029b3715524afbe7c24d3bbf7d32216ff
Nina Athma <nina.athma@gmail.com>
2023-12-12 12:18:15 -0500

    implemented caching for ps5b

--- ps4/routes/ps4.js
+++ ps4/routes/ps4.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
+const redis = require('redis');
 const config = require('../config'); // Adjust the path as needed
 
-const apiKey = config.apiKey;
 
-router.post('/promise', handleWeatherRequest);
+const apiKey = config.apiKey;
+const redisClient = redis.createClient();
+
+
+router.post('/promise', async (req, res) => {
+    const { cityPromise } = req.body;
+
+    // Check if data is in the cache
+    redisClient.get(cityPromise, async (err, cachedData) => {
+        if (err) {
+            console.error('Error accessing Redis cache:', err);
+            return res.status(500).send('Error fetching data');
+        }
+
+        if (cachedData) {
+            // Data found in cache, return it
+            res.json({ data: JSON.parse(cachedData), fromCache: true });
+        } else {
+            // Data not in cache, make API request
+            try {
+                const apiResponse = await makeApiRequest(cityPromise);
+                const responseData = JSON.parse(apiResponse);
+
+                // Cache the response with a 15-second timeout
+                redisClient.setex(cityPromise, 15, JSON.stringify(responseData));
+
+                res.json({ data: responseData, fromCache: false });
+            } catch (error) {
+                res.status(500).send('Error fetching data');
+            }
+        }
+    });
+});
 router.post('/asyncawait', handleWeatherRequestAsyncAwait);
 router.post('/callback', handleWeatherRequestCallback);
 
@@ -14,22 +46,28 @@ function handleWeatherRequest(req, res) {
     makeApiRequestAndRender(res, cityPromise);
 }
 
-async function handleWeatherRequestAsyncAwait(req, res) {
+function handleWeatherRequestAsyncAwait(req, res) {
     const { cityAsyncAwait } = req.body;
     try {
-        const response = await makeApiRequest(cityAsyncAwait);
-        renderTemplateWithData(res, response);
+        makeApiRequest(cityAsyncAwait)
+            .then((response) => sendJsonResponse(res, response))
+            .catch(() => res.status(500).json({ error: 'Error fetching data' }));
     } catch (error) {
-        res.status(500).send('Error fetching data');
+        res.status(500).json({ error: 'Error fetching data' });
     }
 }
 
 function handleWeatherRequestCallback(req, res) {
     const { cityCallback } = req.body;
     makeApiRequest(cityCallback)
-        .then((response) => renderTemplateWithData(res, response))
-        .catch(() => res.status(500).send('Error fetching data'));
+        .then((response) => sendJsonResponse(res, response))
+        .catch(() => res.status(500).json({ error: 'Error fetching data' }));
 }
+
+function sendJsonResponse(res, data) {
+    res.json({ data, fromCache: false });
+}
+
 
 function makeApiRequest(city) {
     const options = {
